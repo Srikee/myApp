1673e3beb527ad949de4c4649fcb7ed7d031fea0
Srikee <kee19384@gmail.com>
2020-08-23 11:21:53 +0700

    เพิ่มหน้ารีสอร์ท

--- src/app/page2/page2.page.ts
+++ src/app/page2/page2.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SessionService } from '../session/session.service';
 
 @Component({
   selector: 'app-page2',
@@ -6,10 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./page2.page.scss'],
 })
 export class Page2Page implements OnInit {
-
-  constructor() { }
-
+  resorts = [];
+  constructor(
+    private session: SessionService
+  ) { }
   ngOnInit() {
+    this.LoadData();
   }
-
+  LoadData() {
+    this.session.ajax("http://localhost/myAppApi/example.php", {}, true).then((res: any) => {
+      this.resorts = res.data;
+    });
+  }
 }
